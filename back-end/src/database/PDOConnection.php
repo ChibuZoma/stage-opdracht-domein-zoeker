44315fb007a956f569ec9c3cb01e64dfa14c5ea5
Chibu68 <62471871+Chibu68@users.noreply.github.com>
2025-06-26 17:40:42 +0200

    Fix: Remove unnecessary echo

--- back-end/src/database/PDOConnection.php
+++ back-end/src/database/PDOConnection.php
@@ -11,7 +11,6 @@ class PDOConnection {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             // Set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
             return $conn;
         }
         catch(PDOException $e) {
